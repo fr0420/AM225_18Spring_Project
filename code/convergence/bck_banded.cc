7b21643b21262a1c37f197b779a32c8d941d7ccf
Rui Fang <fr950420@gmail.com>
2018-07-25 11:24:04 -0400

    Upload code

---
+++ code/convergence/bck_banded.cc
@@ -0,0 +1,161 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <cstring>
+#include <complex>   // std::complex
+#include "omp.h"
+
+typedef std::complex<double> dcomplex;
+
+extern "C" {
+    int zgbsv_(const int *n, const int *kl, const int *ku, const int *nrhs,
+               dcomplex *ab, const int *ldab, int *ipiv, dcomplex *b,
+               const int *ldb, int *info);
+}
+
+
+/** Solves the fully irreversible CV process using backward Euler method with banded solver. */
+
+int main(){
+
+	    // Specify simulation parameters
+        double theta_i = 20.0;
+        double theta_v = -20.0;
+        double sigma = 100;
+        // Store analytical result
+        double true_value = -0.446*sqrt(sigma);
+        
+        double deltaTheta = 0.02;
+        // Calculate other parameters
+        double deltaT = deltaTheta / sigma;
+        double maxT = 2 * fabs(theta_v - theta_i) / sigma;
+        double maxX = 6*sqrt(maxT);
+    #pragma omp parallel for
+    for(int power = 0;power<17;power++){
+
+        // DeltaX is a function of power
+    	double deltaX = 2e-5*pow(2,power);
+    	double deltaTheta = 0.02;
+    	// Calculate other parameters
+    	int n_space = (int)( maxX / deltaX ); // number of spacesteps
+    	int m_time = (int)( maxT / deltaT ); // number of timesteps
+    	// Calculate coefficients for banded solver
+    	double lambda = deltaT / (deltaX*deltaX);
+    	double alpha = -lambda;
+    	double beta = 2.0*lambda + 1.0;
+    	double gamma = -lambda;
+        // Save voltage and current in an array
+        double* data_array = new double[2*m_time];
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+    	// Use banded solver to solve the concentration profile
+
+        // Size of the matrix
+        const int n = n_space;
+        // Number of lower and upper diagonals
+        const int kl = 1, ku =1;
+        // Number of source terms
+        const int nrhs = 1;
+        // Number of rows in banded storage
+        const int ldab = 2*kl+ku+1;
+        // Matrix in banded storage
+        dcomplex* ab = new dcomplex[ldab*n];
+        // Reuse ab
+        dcomplex* ab_temp = new dcomplex[ldab*n];
+        // Source term
+        dcomplex* b = new dcomplex[n];
+        // Pivot storage
+        int* ipiv   = new int[n];
+        // Error flag
+        int info;
+        // Set the dynamic potential 
+    	double Theta = theta_i;
+
+        // Matrix in row-major banded storage
+        // Note that it is easier to construct the matrix on a per-diagonal basis
+        // if we store things in row-major order
+        std::vector<dcomplex> diags(ldab*n, 0.);
+
+        // The first kl rows of banded storage are for internal use by LAPACK
+        std::vector<dcomplex>::iterator start = diags.begin()+n*kl;
+
+        // Construct the diagonals
+        // Refer to LAPACK's banded storage format for details
+        std::fill_n(start+n*kl,         n, dcomplex(beta,0));
+        std::fill_n(start+n*(kl-1)+1, n-1, dcomplex(alpha,0));
+        std::fill_n(start+n*(kl+1),   n-1, dcomplex(gamma,0));
+
+        //Set the boundary conditions
+        diags[n*2] = 1;
+        diags[n+1] = 0;
+
+        diags[n*2+n-1] = 1;
+        diags[(ldab-1)*n+n-2] = 0;
+
+        //Check the matrix construction
+        /*
+        for (int i=0; i<ldab; i++){
+        	for(int j=0;j<n;j++){
+        		printf("%g ",diags[n*i+j].real());
+        	}
+        	printf("\n");
+        }*/
+
+        // Transpose to column-major order for LAPACK
+        for (int i=0; i<ldab; ++i)
+            for (int j=0; j<n; ++j)
+                ab[ldab*j+i] = diags[n*i+j];
+
+
+        memcpy(ab_temp,ab,ldab*n*sizeof(dcomplex));
+        for (int i=0; i<n; ++i) b[i] = dcomplex(1,0);
+        //Boundary condition
+        b[0] = dcomplex(1.0 / (1.0 + exp(-Theta)),0);
+    	b[n-1] = dcomplex(1.0,0);
+/////////////////////////////////////////////////////////////////////////////////
+        //Perform simulation and count elapsed time
+        double t0 = omp_get_wtime();
+        for (int i=0 ; i<m_time;i++){
+
+    		if(i<m_time/2) Theta -= deltaTheta; 
+    		else Theta += deltaTheta; 
+    	    b[0] = dcomplex(1.0 / (1.0 + exp(-Theta)),0);
+    		b[n-1] = dcomplex(1.0,0);
+
+    	    // Call LAPACK
+    	    zgbsv_(&n, &kl, &ku, &nrhs, ab, &ldab, ipiv, b, &n, &info);
+
+    		double flux = -(-b[2].real()+4*b[1].real()-3*b[0].real())/(2*deltaX);
+            data_array[i] = Theta;
+            data_array[m_time+i]= flux;
+    
+    		//reset ab (because zgbsv changes the composition of ab)
+        	memcpy(ab,ab_temp,ldab*n*sizeof(dcomplex));
+
+    	}
+        //Calculate error
+        //store the numerical peak flux
+        double peak_flux = 0;
+
+        for(int j = 0;j<m_time;j++){
+            peak_flux = data_array[m_time+j]<peak_flux?data_array[m_time+j]:peak_flux;
+        }
+
+        double error = (peak_flux-true_value)/true_value;
+
+
+    	double t1 = omp_get_wtime();
+        double time_elapsed = t1-t0;
+        //output dX, error and consumed time
+        printf("%g %g %g\n",deltaX,fabs(error),time_elapsed);
+
+        delete [] ab;
+        delete [] ab_temp;
+        delete [] b;
+        delete [] ipiv;
+        delete [] data_array;
+
+    }
+}
---
+++ code/experiments/make_plots.py
@@ -0,0 +1,59 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+import glob 
+
+"""
+# Snapshots for different k
+srcdir = './snaps_alpha=0.5/'
+files = glob.glob(srcdir+'k=*.out')
+k_list = [float(f.split('/')[-1][2:-4]) for f in files]
+data_list = [np.loadtxt(f) for f in files]
+snaps = data_list[0].shape[1]-2
+xx = data_list[0][:, 0]
+
+target_k = [900, 50, 2, 0.15625]
+k_data_list = []
+
+for k, data in zip(k_list, data_list):
+	if k in target_k: 
+		k_data_list.append((k, data))
+
+colors = pl.cm.jet(np.linspace(0.3,0.7,len(k_data_list)))
+
+for i_snap in range(1, snaps+1): 
+	plt.figure()
+	for i, (k, data) in enumerate(k_data_list):
+		plt.plot(xx, data[:, i_snap+1], color=colors[i], label=r'$\kappa^0=%s$' % k)
+	plt.ylim(-0.05, 1.05)
+	plt.xlim(xx.min(), xx.max())
+	plt.xlabel(r'$X$')
+	plt.ylabel(r'$C_O$')
+	plt.title(str(i_snap/snaps)+r'$T_{max}$')
+	plt.legend(loc='lower right')
+	plt.savefig(srcdir+'snap'+str(i_snap)+'.png', dpi=300)
+"""
+
+
+# Snapshots for different alpha 
+srcdir = './snaps_k=50/'
+files = glob.glob(srcdir+'alpha=*.out')
+alpha_list = [float(f.split('/')[-1][6:-4]) for f in files]
+data_list = [np.loadtxt(f) for f in files]
+snaps = data_list[0].shape[1]-2
+xx = data_list[0][:, 0]
+
+colors = pl.cm.jet(np.linspace(0,1,len(alpha_list)))
+
+for i_snap in range(1, snaps+1): 
+	plt.figure()
+	for i, (alpha, data) in enumerate(zip(alpha_list, data_list)):
+		plt.plot(xx, data[:, i_snap+1], color=colors[i], label=r'$\alpha=%s$' % alpha)
+	plt.ylim(-0.05, 1.05)
+	plt.xlim(xx.min(), xx.max())
+	plt.xlabel(r'$X$')
+	plt.ylabel(r'$C_O$')
+	plt.title(r'$%s T_{max}$' % (i_snap/snaps))
+	plt.legend(loc='lower right')
+	plt.savefig(srcdir+'snap'+str(i_snap)+'.png', dpi=300)
+# plt.show()
